ca057abec313fb1ed8c580a4336270b59f6d95c9
Juraj Michalek <juraj.michalek@gmail.com>
2024-12-10 15:22:25 +0100

    update implementation to new hal

--- README.md
+++ README.md
@@ -8,58 +8,49 @@ Add dependencies to the project:
 
 ```
 cargo add esp-display-interface-spi-dma
-cargo add static_cell
-cargo add esp-bsp
 ```
 
 ## Code
 
 Example for ESP32-S3-BOX:
 
-```
-use static_cell::make_static;
+```rust
 use esp_display_interface_spi_dma::display_interface_spi_dma;
-...
-
-let dma = Gdma::new(peripherals.DMA);
-let dma_channel = dma.channel0;
-let descriptors = make_static!([0u32; 8 * 3]);
-let rx_descriptors = make_static!([0u32; 8 * 3]);
-let (lcd_sclk, lcd_mosi, lcd_cs, lcd_miso, lcd_dc, mut lcd_backlight, lcd_reset) = lcd_gpios!(BoardType::ESP32S3Box, io);
-
-let spi = Spi::new(
-    peripherals.SPI2,
-    40u32.MHz(),
-    SpiMode::Mode0,
-    &clocks
-).with_pins(
-    Some(lcd_sclk),
-    Some(lcd_mosi),
-    Some(lcd_miso),
-    Some(lcd_cs),
-).with_dma(
-    dma_channel.configure(
-        false,
-        &mut *descriptors,
-        &mut *rx_descriptors,
-        DmaPriority::Priority0,
+//  ...
+fn display_code() {
+    let spi = Spi::new_with_config(
+        peripherals.SPI2,
+        esp_hal::spi::master::Config {
+            frequency: 40u32.MHz(),
+            ..esp_hal::spi::master::Config::default()
+        },
     )
-);
+    .with_sck(lcd_sclk)
+    .with_mosi(lcd_mosi)
+    .with_miso(lcd_miso)
+    .with_cs(lcd_cs)
 
+    .with_dma(dma_channel.configure(false, DmaPriority::Priority0));
+    
+    let di = display_interface_spi_dma::new_no_cs(LCD_MEMORY_SIZE, spi, lcd_dc);
 
-let di = display_interface_spi_dma::new_no_cs(2 * 256 * 192, spi, lcd_dc);
+    let mut display = mipidsi::Builder::new(mipidsi::models::ILI9341Rgb565, di)
+        .display_size(240, 320)
+        .orientation(mipidsi::options::Orientation::new())
+        .color_order(mipidsi::options::ColorOrder::Bgr)
+        .reset_pin(lcd_reset)
+        .init(&mut delay)
+        .unwrap();
 
-let display_config = DisplayConfig::for_board(BoardType::ESP32S3Box);
-let mut display = match mipidsi::Builder::ili9342c_rgb565(di)
-    .with_display_size(display_config.h_res, display_config.v_res)
-    .with_orientation(mipidsi::Orientation::PortraitInverted(false))
-    .with_color_order(mipidsi::ColorOrder::Bgr)
-    .init(&mut delay, Some(lcd_reset))
-{
-    Ok(display) => display,
-    Err(_e) => {
-        // Handle the error and possibly exit the application
-        panic!("Display initialization failed");
-    }
-};
+    let _ = lcd_backlight.set_high();
+
+    println!("Initializing...");
+    Text::new(
+        "Initializing...",
+        Point::new(80, 110),
+        MonoTextStyle::new(&FONT_8X13, RgbColor::WHITE),
+    )
+    .draw(&mut display)
+    .unwrap();
+}
 ```
--- src/display_interface_spi_dma.rs
+++ src/display_interface_spi_dma.rs
@@ -1,62 +1,33 @@
 //! DMA SPI interface for display drivers
 
 use core::cell::RefCell;
-
-#[cfg(feature = "esp32")]
-use esp32_hal as hal;
-#[cfg(feature = "esp32c3")]
-use esp32c3_hal as hal;
-#[cfg(feature = "esp32c6")]
-use esp32c6_hal as hal;
-#[cfg(feature = "esp32s2")]
-use esp32s2_hal as hal;
-#[cfg(feature = "esp32s3")]
-use esp32s3_hal as hal;
+use core::ptr::addr_of_mut;
 
 use byte_slice_cast::AsByteSlice;
 use display_interface::{DataFormat, DisplayError, WriteOnlyDataCommand};
-use hal::gpio::{Output, OutputPin, PushPull};
-use hal::spi::master::dma::SpiDmaTransfer;
-use hal::spi::master::InstanceDma;
-use hal::{
-    dma::{ChannelTypes, SpiPeripheral},
-    prelude::_esp_hal_dma_DmaTransfer,
-    spi::{DuplexMode, IsFullDuplex},
+use esp_hal::{
+    dma::{DmaDescriptor, DmaTxBuf},
+    gpio::Output,
+    spi::master::SpiDmaTransfer,
 };
 
 const DMA_BUFFER_SIZE: usize = 4096;
-type SpiDma<'d, T, C, M> = hal::spi::master::dma::SpiDma<'d, T, C, M>;
+type SpiDma<'d> = esp_hal::spi::master::SpiDma<'d, esp_hal::Blocking>;
 
 /// SPI display interface.
 ///
 /// This combines the SPI peripheral and a data/command as well as a chip-select pin
-pub struct SPIInterface<'d, DC, CS, T, C, M>
-where
-    DC: OutputPin,
-    CS: OutputPin,
-    T: InstanceDma<C::Tx<'d>, C::Rx<'d>>,
-    C: ChannelTypes,
-    C::P: SpiPeripheral,
-    M: DuplexMode,
-{
+pub struct SPIInterface<'d> {
     avg_data_len_hint: usize,
-    spi: RefCell<Option<SpiDma<'d, T, C, M>>>,
-    transfer: RefCell<Option<SpiDmaTransfer<'d, T, C, &'static mut [u8], M>>>,
-    dc: DC,
-    cs: Option<CS>,
+    spi: RefCell<Option<SpiDma<'d>>>,
+    transfer: RefCell<Option<SpiDmaTransfer<'d, esp_hal::Blocking, DmaTxBuf>>>,
+    dc: Output<'d>,
+    cs: Option<Output<'d>>,
 }
 
 #[allow(unused)]
-impl<'d, DC, CS, T, C, M> SPIInterface<'d, DC, CS, T, C, M>
-where
-    DC: OutputPin,
-    CS: OutputPin,
-    T: InstanceDma<C::Tx<'d>, C::Rx<'d>>,
-    C: ChannelTypes,
-    C::P: SpiPeripheral,
-    M: DuplexMode,
-{
-    pub fn new(avg_data_len_hint: usize, spi: SpiDma<'d, T, C, M>, dc: DC, cs: CS) -> Self {
+impl<'d> SPIInterface<'d> {
+    pub fn new(avg_data_len_hint: usize, spi: SpiDma<'d>, dc: Output<'d>, cs: Output<'d>) -> Self {
         Self {
             avg_data_len_hint,
             spi: RefCell::new(Some(spi)),
@@ -66,21 +37,9 @@ where
         }
     }
 
-    /// Consume the display interface and return
-    /// the underlying peripheral driver and GPIO pins used by it
-    pub fn release(self) -> (SpiDma<'d, T, C, M>, DC, Option<CS>) {
-        (self.spi.take().unwrap(), self.dc, self.cs)
-    }
-
-    fn send_u8(&mut self, words: DataFormat<'_>) -> Result<(), DisplayError>
-    where
-        T: InstanceDma<C::Tx<'d>, C::Rx<'d>>,
-        C: ChannelTypes,
-        C::P: SpiPeripheral,
-        M: DuplexMode + IsFullDuplex,
-    {
+    fn send_u8(&mut self, words: DataFormat<'_>) -> Result<(), DisplayError> {
         if let Some(transfer) = self.transfer.take() {
-            let (_, reclaimed_spi) = transfer.wait().unwrap();
+            let (reclaimed_spi, _) = transfer.wait();
             self.spi.replace(Some(reclaimed_spi));
         }
 
@@ -139,12 +98,10 @@ where
         Ok(())
     }
 
-    fn single_transfer(&mut self, send_buffer: &'static mut [u8])
-    where
-        M: DuplexMode + IsFullDuplex,
-    {
-        let transfer = self.spi.take().unwrap().dma_write(send_buffer).unwrap();
-        let (_, reclaimed_spi) = transfer.wait().unwrap();
+    fn single_transfer(&mut self, send_buffer: &'static mut [u8]) {
+        let mut buffer = DmaTxBuf::new(descriptors(), send_buffer).unwrap();
+        let transfer = self.spi.take().unwrap().write(buffer).unwrap();
+        let (reclaimed_spi, _) = transfer.wait();
         self.spi.replace(Some(reclaimed_spi));
     }
 
@@ -154,13 +111,12 @@ where
         convert: fn(WORD) -> <WORD as num_traits::ToBytes>::Bytes,
     ) where
         WORD: num_traits::int::PrimInt + num_traits::ToBytes,
-        M: DuplexMode + IsFullDuplex,
     {
         let mut desired_chunk_sized =
             self.avg_data_len_hint - ((self.avg_data_len_hint / DMA_BUFFER_SIZE) * DMA_BUFFER_SIZE);
         let mut spi = Some(self.spi.take().unwrap());
         let mut current_buffer = 0;
-        let mut transfer: Option<SpiDmaTransfer<'d, T, C, _, M>> = None;
+        let mut transfer: Option<SpiDmaTransfer<'d, esp_hal::Blocking, DmaTxBuf>> = None;
         loop {
             let buffer = if current_buffer == 0 {
                 &mut dma_buffer1()[..]
@@ -192,7 +148,7 @@ where
 
             if let Some(transfer) = transfer {
                 if idx > 0 {
-                    let (relaimed_buffer, reclaimed_spi) = transfer.wait().unwrap();
+                    let (reclaimed_spi, relaimed_buffer) = transfer.wait();
                     spi = Some(reclaimed_spi);
                 } else {
                     // last transaction inflight
@@ -201,61 +157,47 @@ where
             }
 
             if idx > 0 {
-                transfer = Some(spi.take().unwrap().dma_write(&mut buffer[..idx]).unwrap());
+                let mut dma_buffer = DmaTxBuf::new(descriptors(), buffer).unwrap();
+                dma_buffer.set_length(idx);
+                transfer = Some(spi.take().unwrap().write(dma_buffer).unwrap());
                 current_buffer = (current_buffer + 1) % 2;
             } else {
                 break;
             }
         }
-        self.spi.replace(spi);
     }
 }
 
-pub fn new_no_cs<'d, DC, T, C, M>(
+pub fn new_no_cs<'d>(
     avg_data_len_hint: usize,
-    spi: SpiDma<'d, T, C, M>,
-    dc: DC,
-) -> SPIInterface<'d, DC, hal::gpio::Gpio0<Output<PushPull>>, T, C, M>
-where
-    DC: OutputPin,
-    T: InstanceDma<C::Tx<'d>, C::Rx<'d>>,
-    C: ChannelTypes,
-    C::P: SpiPeripheral,
-    M: DuplexMode,
-{
+    spi: SpiDma<'d>,
+    dc: Output<'d>,
+) -> SPIInterface<'d> {
     SPIInterface {
         avg_data_len_hint,
         spi: RefCell::new(Some(spi)),
         transfer: RefCell::new(None),
         dc,
-        cs: None::<hal::gpio::Gpio0<Output<PushPull>>>,
+        cs: None,
     }
 }
 
-impl<'d, DC, CS, T, C, M> WriteOnlyDataCommand for SPIInterface<'d, DC, CS, T, C, M>
-where
-    DC: OutputPin + hal::prelude::_embedded_hal_digital_v2_OutputPin,
-    CS: OutputPin + hal::prelude::_embedded_hal_digital_v2_OutputPin,
-    T: InstanceDma<C::Tx<'d>, C::Rx<'d>>,
-    C: ChannelTypes,
-    C::P: SpiPeripheral,
-    M: DuplexMode + IsFullDuplex,
-{
+impl<'d> WriteOnlyDataCommand for SPIInterface<'d> {
     fn send_commands(&mut self, cmds: DataFormat<'_>) -> Result<(), DisplayError> {
         // Assert chip select pin
         if let Some(cs) = self.cs.as_mut() {
-            cs.set_low().map_err(|_| DisplayError::CSError)?;
+            cs.set_low();
         }
 
         // 1 = data, 0 = command
-        self.dc.set_low().map_err(|_| DisplayError::DCError)?;
+        self.dc.set_low();
 
         // Send words over SPI
         let res = self.send_u8(cmds);
 
         // Deassert chip select pin
         if let Some(cs) = self.cs.as_mut() {
-            cs.set_high().ok();
+            cs.set_high();
         }
 
         res
@@ -264,30 +206,35 @@ where
     fn send_data(&mut self, buf: DataFormat<'_>) -> Result<(), DisplayError> {
         // Assert chip select pin
         if let Some(cs) = self.cs.as_mut() {
-            cs.set_low().map_err(|_| DisplayError::CSError)?;
+            cs.set_low();
         }
 
         // 1 = data, 0 = command
-        self.dc.set_high().map_err(|_| DisplayError::DCError)?;
+        self.dc.set_high();
 
         // Send words over SPI
         let res = self.send_u8(buf);
 
         // Deassert chip select pin
         if let Some(cs) = self.cs.as_mut() {
-            cs.set_high().ok();
+            cs.set_high();
         }
 
         res
     }
 }
 
+fn descriptors() -> &'static mut [DmaDescriptor; 8 * 3] {
+    static mut DESCRIPTORS: [DmaDescriptor; 8 * 3] = [DmaDescriptor::EMPTY; 8 * 3];
+    unsafe { &mut *addr_of_mut!(DESCRIPTORS) }
+}
+
 fn dma_buffer1() -> &'static mut [u8; DMA_BUFFER_SIZE] {
     static mut BUFFER: [u8; DMA_BUFFER_SIZE] = [0u8; DMA_BUFFER_SIZE];
-    unsafe { &mut BUFFER }
+    unsafe { &mut *addr_of_mut!(BUFFER) }
 }
 
 fn dma_buffer2() -> &'static mut [u8; DMA_BUFFER_SIZE] {
     static mut BUFFER: [u8; DMA_BUFFER_SIZE] = [0u8; DMA_BUFFER_SIZE];
-    unsafe { &mut BUFFER }
+    unsafe { &mut *addr_of_mut!(BUFFER) }
 }
